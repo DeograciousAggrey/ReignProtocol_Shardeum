ef00e36f355fac4d400d6e5fdb7525cdf9046178
DeograciousAggrey <deograciousaggrey@gmail.com>
2024-04-14 01:01:27 +0300

    Updated the staking contract

--- smartcontracts/contracts/protocol/old/AuthorizeUser.sol
+++ smartcontracts/contracts/protocol/old/AuthorizeUser.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.4;
 
-import "../../interfaces/IAuthorizeUser.sol";
+import {IAuthorizeUser} from "../../interfaces/IAuthorizeUser.sol";
 import {BaseUpgradeablePausable} from "../BaseUpgradeablePausable.sol";
 import {ReignConfig} from "../ReignConfig.sol";
 import {ConfigHelper} from "../ConfigHelper.sol";
@@ -13,6 +13,7 @@ contract AuthorizeUser is BaseUpgradeablePausable, IAuthorizeUser {
     /////////  ERROR MESSAGES    ////////////////
     /////////////////////////////////////////////
     error UnauthorizedUser__InvalidAddress();
+    error UnauthorizedUser__NotAuthorized();
 
     ReignConfig private reignConfig;
     using ConfigHelper for ReignConfig;
@@ -31,6 +32,31 @@ contract AuthorizeUser is BaseUpgradeablePausable, IAuthorizeUser {
         _setupRole(Constants.getAuthorizeUserRole(), owner);
         _setRoleAdmin(Constants.getAuthorizeUserRole(), Constants.getAdminRole());
         
+    }
+
+    modifier onlyAuthorizeUserRole(){
+        if(!hasRole(Constants.getAuthorizeUserRole(), msg.sender)){
+            revert UnauthorizedUser__NotAuthorized();
+        }
+        _;
+    }
+
+    function addToAuthorized(address _user) external override onlyAuthorizeUserRole{
+        if(_user == address(0)) revert UnauthorizedUser__InvalidAddress();
+        authorizedUserList[_user] = true;
+        emit AuthorizedUserListed(_user);
+
+    }
+
+    function removeFromAuthorized(address _user) external override onlyAuthorizeUserRole{
+        if(_user == address(0)) revert UnauthorizedUser__InvalidAddress();
+        authorizedUserList[_user] = false;
+        emit AuthorizedUserUnlisted(_user);
+    }
+
+    function isAuthorized(address _user) external view override returns(bool){
+        if(_user == address(0)) revert UnauthorizedUser__InvalidAddress();
+        return authorizedUserList[_user];
     }
 
 }
